d58ae42c470252bdd92c659039a6d6f6050dbf67
Thomas Forrer <thomas.forrer@wuerth-phoenix.com>
2021-02-18 19:51:48 +0100

    trigger __toString method

--- php/e05/example3.php
+++ php/e05/example3.php
@@ -1,5 +1,23 @@
 <?php
 
+class SQL_Row_Value
+{
+    private $_table;
+
+    // some PHP code...
+
+    function getValue($id=0)
+    {
+        $sql = "SELECT * FROM {$this->_table} WHERE id = " . (int)$id;
+//        $result = mysql_query($sql, $DBFactory::getConnection());
+//        $row = mysql_fetch_assoc($result);
+
+//        return $row['value'];
+        print "SQL executed:{$sql}\n";
+        return $sql;
+    }
+}
+
 class Example3
 {
     protected $obj;
@@ -18,5 +36,6 @@ class Example3
 // some PHP code...
 
 $user_data = unserialize($_POST['data']);
+$trigger_the_tostring_converting_object_to_string = "received data: " . $user_data ;
 
 // some PHP code...
